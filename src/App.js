a8b25e01b4496d3b7dfece36abba4aa806b361e9
venkatasridhar <pvsridharroyal@gmail.com>
2023-03-03 18:26:35 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -9,15 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1>Hello World</h1>
+        
+        <h1>Hello World new</h1>
+        <p>New World</p>
       </header>
     </div>
   );
